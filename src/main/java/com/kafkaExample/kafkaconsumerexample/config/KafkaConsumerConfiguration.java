72fd42395b1b9756bfeaae879e0cb57a3a34a7b8
nawaz462 <shahbaznawaz786@gmail.com>
2018-11-26 15:02:05 +0530

    changes in code

--- src/main/java/com/kafkaExample/kafkaconsumerexample/config/KafkaConsumerConfiguration.java
+++ src/main/java/com/kafkaExample/kafkaconsumerexample/config/KafkaConsumerConfiguration.java
@@ -34,7 +34,7 @@ public class KafkaConsumerConfiguration {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- src/main/java/com/kafkaExample/kafkaconsumerexample/listener/KafkaConsumer.java
+++ src/main/java/com/kafkaExample/kafkaconsumerexample/listener/KafkaConsumer.java
@@ -2,28 +2,52 @@ package com.kafkaExample.kafkaconsumerexample.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
 import com.kafkaExample.kafkaconsumerexample.model.User;
 
-@Service
+@Component
 public class KafkaConsumer {
 	
-	Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
-
+	private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+	
+	@Value("${kafka.destination.topic.name}")
+	private String destinationTopic;
+	
+	@Autowired
+	private KafkaTemplate<String, User> kafkaSender;
+	
     @KafkaListener(topics = "Kafka_Example", group = "group_id")
     public void consume(String message) {
         System.out.println("Consumed  message: " + message);
         log.info("Consumed  message: " + message);
     }
 
-
-    @KafkaListener(topics = "Kafka_Json_Example", group = "group_json",
+    /*
+     * 
+     * Method which consumes the JSON message from kafka topic and
+     * sends it to other topic after processing
+     */
+    @KafkaListener(topics = "${kafka.source.topic.name}", group = "group_json",
             containerFactory = "userKafkaListenerFactory")
     public void consumeJson(User user) {
-        System.out.println("Consumed JSON Message: " + user);
-        //processing
-        //publish it to other topic
+    	
+    	 log.info("Consumed JSON Message: " + user);
+    	 System.out.println("Consumed JSON  message: " + user);
+        //processing - updating name from the consumed message
+        user.setName(user.getName()+"_Updated");
+        
+        try {
+        	 kafkaSender.send(destinationTopic, user);
+             log.info("Message published successfully ");
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        	log.error("Message publishing failed");
+        }
     }
 }
